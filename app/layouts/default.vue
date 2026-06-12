91d85fa95b5ffb5a321f0aa78c365ba2e5610266
Danya Cherkesov <daniil_cherkesov@studio-aw.ru>
2026-06-12 13:43:46 +0300

    revert(control-center): drop ambient glow; keep frosted-glass material only

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,7 +5,6 @@ useNotificationsSse()
 <template>
   <div class="relative h-screen flex overflow-hidden">
     <div class="pointer-events-none absolute -top-40 -right-24 size-[40rem] rounded-full dark:bg-accent-500/15 blur-[130px]" />
-    <div class="pointer-events-none fixed -top-2 left-1/2 -translate-x-1/2 z-[90] w-[380px] h-[84px] rounded-full bg-accent-500/35 dark:bg-accent-500/40 blur-xl" />
     <AppSidebar />
     <div class="flex-1 flex flex-col min-w-0">
       <ControlCenter />
